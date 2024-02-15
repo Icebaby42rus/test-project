d6ee3d5f6d8d3926a0a1ee61da20646e8d24b147
Icebaby42rus <icebaby42rus@gmail.com>
2024-02-15 16:09:38 +0700

    add ui section

--- src/index.js
+++ src/index.js
@@ -1,18 +1,56 @@
 import './index.html'
 import './index.scss'
 
-const menu = document.querySelector('.header__menu');
-const menuBtn = document.querySelector('.header__menu-btn');
+// MENU
+const menu = document.querySelector('.header__menu')
+const menuBtn = document.querySelector('.header__menu-btn')
 
-menuBtn.addEventListener('click', function() {
-  menu.classList.toggle("opened");
-  // menuList.classList.toggle('menu__list_visible');
-});
+menuBtn.addEventListener('click', function () {
+	menu.classList.toggle('opened')
+})
 
-// const menuBtn = document.querySelector('.header__menu');
-// const menuContent = document.querySelector('.menu__list');
+// TABS
+document.addEventListener('DOMContentLoaded', function () {
+	const tabs = document.querySelectorAll('.tabs-navigation input[type="radio"]')
+	const tabContents = document.querySelectorAll('.tab-content')
 
-// menuBtn.addEventListener('click', () => {
-//   menuBtn.classList.toggle('menu__burger_open');
-//   menuList.classList.toggle('menu__list_visible');
-// });
+  tabs[0].checked = true;
+
+	tabs.forEach(tab => {
+		tab.addEventListener('change', function () {
+			const targetId = this.getAttribute('id').replace('tab-', '')
+			tabContents.forEach(content => {
+				if (content.getAttribute('id') === targetId + '-content') {
+					content.classList.add('active')
+				} else {
+					content.classList.remove('active')
+				}
+			})
+		})
+	})
+})
+
+// INPUT
+let input = document.getElementById('general-field')
+
+input.oninput = function() {
+  document.querySelector('.general-field').style.borderBottomWidth = 4 + "px";
+  document.querySelector('.general-field').style.borderBottomColor = "#3E29E3";
+};
+
+input.onblur = function() {
+  document.querySelector('.general-field').style.borderBottomWidth = 2 + "px";
+  document.querySelector('.general-field').style.borderBottomColor = "#AEADAE";
+
+  if (!input.value.includes('@')) {
+    document.querySelector('.general-field').classList.add('invalid');
+    document.querySelector('.general-field').style.borderBottomWidth = 4 + "px";
+    document.querySelector('.general-field').style.borderBottomColor = "#E80F3B";
+  }
+}
+
+input.onfocus = function() {
+  if (document.querySelector('.general-field').classList.contains('invalid')) {
+    document.querySelector('.general-field').classList.remove('invalid');
+  }
+};
